f2e2c5450b97a95c8414ad5ee56a5f814f585cac
Peter Gibbons <gibbons.peter312@gmail.com>
2022-05-09 16:57:11 -0700

    Update app.py

--- processing/app.py
+++ processing/app.py
@@ -51,7 +51,7 @@ def create_tables():
     ''')
     conn.commit()
     conn.close()
-if path.exists(app_config["datastore"]["filename"]):
+if not path.exists(app_config["datastore"]["filename"]):
     create_tables()
     print('created db')
 
