729e5249be2e107c7efc5edb6e1f84c1ec4464e5
Peter Gibbons <gibbons.peter312@gmail.com>
2022-05-16 12:41:27 -0700

    Update app.py

--- storage/app.py
+++ storage/app.py
@@ -93,7 +93,7 @@ def get_user(timestamp):
     timestamp_datetime = datetime.datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%SZ")
     scores = session.query(User).filter(User.timeStamp >= timestamp_datetime)
     test = session.query(User)
-    print(test)
+    print(f'test result: {test}')
     result_list = []
 
     for i in scores:
