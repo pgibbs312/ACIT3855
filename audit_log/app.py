e90e7a03bf080e9761f5f2ce7e1b7734719a0085
Peter Gibbons <gibbons.peter312@gmail.com>
2022-05-18 14:40:26 -0700

    Update app.py

--- audit_log/app.py
+++ audit_log/app.py
@@ -54,19 +54,39 @@ def get_score(index):
 def get_user(index):
 
     """ Get User Reading in History """
+    retry_count = 0
     hostname = f'{app_config["events"]["hostname"]}:{app_config["events"]["port"]}'
-    client = KafkaClient(hosts=hostname)
-    topic = client.topics[str.encode(app_config["events"]["topic"])]
-    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    while retry_count < app_config["kafka_connect"]["retry_count"]:
+        try:
+            logger.info('trying to connect, attempt: %d' % (retry_count))
+            print(hostname)
+            client = KafkaClient(hosts=hostname) 
+            topic = client.topics[str.encode(app_config['events']['topic'])] 
+            consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+        except:
+            logger.info('attempt %d failed, retry in 5 seoncds' % (retry_count))
+            retry_count += 1
+            sleep(app_config["kafka_connect"]["sleep_time"])
+        else:
+            break
+    logger.info('connected to kafka')
+    
+    
+    index = int(index)
     logger.info("Retrieving user at index %d" % index)
-    try: 
+    try:
+        user_list = []
         for msg in consumer:
             msg_str = msg.value.decode('utf-8')
             msg = json.loads(msg_str)
+            if msg['type'] == 'add_user':
+                user_list.append(msg)
+        logger.info('returned payload with trace_id: %s' % (user_list[index]['payload']['trace_id']))
+        return user_list[index], 200
     except:
         logger.error("No more messages found")
     
-    logger.error("Could not find score at index %d" % index)
+    logger.error("Could not find user at index %d" % index)
     return { "message": "Not found "}, 404
 
 app = connexion.FlaskApp(__name__, specification_dir='')
